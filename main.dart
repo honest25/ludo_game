4178ddc2bbbf49e4117a1c0ed2834d001d60cf38
honest25 <116367876+honest25@users.noreply.github.com>
2026-03-12 22:37:41 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,17 +1,7 @@
-import 'package:flutter/material.dart';
-import 'ui/splash_screen.dart';
-
 void main() {
-  runApp(LudoApp());
-}
 
-class LudoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ludo Multiplayer',
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
-  }
+  SocketService().connect();
+
+  runApp(LudoApp());
+
 }
