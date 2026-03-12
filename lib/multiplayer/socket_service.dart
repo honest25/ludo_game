e5422b923095fe9ef5dff2d9daad344a12926830
honest25 <116367876+honest25@users.noreply.github.com>
2026-03-12 22:32:37 +0530

    Update socket_service.dart

--- lib/multiplayer/socket_service.dart
+++ lib/multiplayer/socket_service.dart
@@ -2,38 +2,40 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
 
+  static final SocketService _instance = SocketService._internal();
+
+  factory SocketService() {
+    return _instance;
+  }
+
+  SocketService._internal();
+
   late IO.Socket socket;
 
   void connect() {
 
     socket = IO.io(
-      "https://ludo-backend.onrender.com",
+      "https://ludo-multiplayer-server.onrender.com",
       IO.OptionBuilder()
           .setTransports(['websocket'])
+          .disableAutoConnect()
           .build(),
     );
 
     socket.connect();
 
     socket.onConnect((_) {
-      print("Connected to server");
+      print("✅ Connected to Ludo Server");
     });
-  }
 
-  void createRoom() {
-    socket.emit("create_room");
-  }
+    socket.onDisconnect((_) {
+      print("❌ Disconnected from Server");
+    });
 
-  void joinRoom(String roomId) {
-    socket.emit("join_room", roomId);
-  }
+    socket.onConnectError((err) {
+      print("Connection Error: $err");
+    });
 
-  void rollDice() {
-    socket.emit("roll_dice");
-  }
-
-  void moveToken(data) {
-    socket.emit("move_token", data);
   }
 
 }
