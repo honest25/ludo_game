4934870de496156af7c20f9f507dfefd481e65df
honest25 <116367876+honest25@users.noreply.github.com>
2026-03-12 22:13:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,67 @@
-const express = require("express")
-const http = require("http")
-const socketio = require("socket.io")
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const cors = require("cors");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
 
-let rooms = {}
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET","POST"]
+  }
+});
+
+let rooms = {};
+
+app.get("/", (req,res)=>{
+  res.send("Ludo Multiplayer Server Running");
+});
 
 io.on("connection", socket => {
 
-    console.log("Player connected")
+  console.log("Player connected:", socket.id);
 
-    socket.on("createRoom", () => {
+  socket.on("createRoom", () => {
 
-        let roomCode = Math.random().toString(36).substring(7)
+    let roomCode = Math.random().toString(36).substring(7);
 
-        rooms[roomCode] = []
+    rooms[roomCode] = [];
 
-        rooms[roomCode].push(socket.id)
+    rooms[roomCode].push(socket.id);
 
-        socket.join(roomCode)
+    socket.join(roomCode);
 
-        socket.emit("roomCreated", roomCode)
+    socket.emit("roomCreated", roomCode);
 
-    })
+  });
 
-    socket.on("joinRoom", (code) => {
+  socket.on("joinRoom", (code) => {
 
-        if(rooms[code]){
+    if(rooms[code]){
 
-            rooms[code].push(socket.id)
+      rooms[code].push(socket.id);
 
-            socket.join(code)
+      socket.join(code);
 
-            io.to(code).emit("playerJoined")
+      io.to(code).emit("playerJoined");
 
-        }
+    }
 
-    })
+  });
 
-})
+  socket.on("disconnect", () => {
+    console.log("Player disconnected");
+  });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
